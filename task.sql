86984ba0672abbc494fb914105fe3e3ced36dfea
sora <illia.bilik143@gmail.com>
2026-06-08 11:36:58 +0300

    Solution_1

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 USE ShopDB;
 
-CREATE INDEX Name ON Products(Name);
+CREATE INDEX idx_product_name ON Products(Name);
 
-CREATE INDEX Email ON Customers(Email);
+CREATE INDEX idx_customers_email ON Customers(Email);
